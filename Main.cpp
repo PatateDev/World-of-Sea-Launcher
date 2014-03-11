5ec6ffb0d5b6ddb88b0f122d3a8e66f8be7b3558
mathdu07 <mathdu07.minecraft@gmail.com>
2014-03-11 01:00:43 +0100

    Add login request

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,80 @@
 #include "Main.h"
 
+bool login(false);
+sf::String username, password, session;
+LoginErrorId error(NONE);
+int loopLeft(10);
+
 void connect()
 {
+    sf::TcpSocket socket;
+    std::cout << "Connecting to the login server ..." << std::endl;
+    
+    if (socket.connect(LOGIN_IP, LOGIN_PORT) == sf::Socket::Done)
+    {
+        sf::Packet packet;
+        packet << (sf::Uint8) LOGIN << username << password;
+        socket.send(packet);
+        
+        sf::Packet loginResponse;
+        socket.receive(loginResponse);
+        sf::Uint8 packetId;
+        loginResponse >> packetId;
+        
+        if (packetId == SUCCESS)
+        {
+            login = true;
+            loginResponse >> session;
+            std::cout << "Logged in !" << std::endl;
+        }
+        else if (packetId == FAIL)
+        {
+            sf::Uint8 errorId;
+            loginResponse >> errorId;
+            error = static_cast<LoginErrorId>(errorId);
+            std::cout << "Login fail, error : " << (short) errorId << std::endl;
+        } 
+        else
+        {
+            error = BAD_REQUEST;
+        }
+    }
+    else
+    {
+        error = INTERNAL_ERROR;
+        std::cout << "Can't connect to the server" << std::endl;
+    }
+}
 
+void launch()
+{
+    //TODO
+    std::cout << "Launching world-of-sea-client with session : " << session.toAnsiString() << std::endl;
+}
+
+sf::String getErrorMessage()
+{
+    switch(error)
+    {
+    case NONE:
+        return "";
+    case INTERNAL_ERROR:
+        return "An error has occured, is the server running ?";
+    case UNKNOWN_USER:
+        return "Uknown username, are you registered ?";
+    case BAD_PASSWORD:
+        return "The given password is wrong";
+    case BAD_REQUEST:
+        return "Bad request sent/received, is the launcher up to date ?";
+    default:
+        return "Unknown error";
+    }
 }
 
 int main(int argc, char*argv[])
 {
     // DECLARATION
-    sf::RenderWindow window(sf::VideoMode(LAUNCHER_WIDTH_X, LAUNCHER_HEIGHT_Y), "World of Sea");
+    sf::RenderWindow window(sf::VideoMode(LAUNCHER_WIDTH_X, LAUNCHER_HEIGHT_Y), "World of Sea", sf::Style::Titlebar | sf::Style::Close);
     sf::Event event;
     sf::Font fontArial, fontCaribbean, fontFlogged;
     sf::Texture textureTextfield, textureTextfieldFocus, textureBackground, textureButton, textureButtonFocus;
@@ -77,6 +143,11 @@ int main(int argc, char*argv[])
     sf::ui::Button button(connect, textureButton, textureButtonFocus, textureButtonFocus);
 	button.move(LAUNCHER_WIDTH_X/2.5, LAUNCHER_HEIGHT_Y/1.5);
 	//
+	
+	//Result label
+	sf::ui::Label result(fontCaribbean, "");
+    result.move(LAUNCHER_WIDTH_X/3, LAUNCHER_HEIGHT_Y/1.5 + 60);
+    result.setFontSize(18);
 
     while (window.isOpen())
 	{
@@ -91,6 +162,9 @@ int main(int argc, char*argv[])
 
         button.updateEvent(event);
         //
+        
+        username = textfieldLogin.getText();
+        password = textfieldPassword.getText();
 
         // Output
         window.clear(sf::Color::White);
@@ -104,10 +178,32 @@ int main(int argc, char*argv[])
         window.draw(textfieldPassword);
 
         window.draw(button);
+        
+        window.draw(result);
 
         window.display();
         //
+        
+        if (login)
+        {
+            result.setFontColor(sf::Color::Green);
+            result.setText("Logged in successfully");
+            window.display();
+            
+            if (loopLeft <= 0)
+                window.close();
+            else
+                loopLeft--;
+        }
+        else
+        {
+            result.setFontColor(sf::Color::Red);
+            result.setText(getErrorMessage());
+        }
 	}
+	
+	if (login)
+	    launch();
 
     return 0;
 }
--- Main.h
+++ Main.h
@@ -3,10 +3,31 @@
 
 #include <SFML/UI.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 #include <iostream>
 #include <cstdlib>
 
 #define LAUNCHER_WIDTH_X 800
 #define LAUNCHER_HEIGHT_Y 640
+
+#define LOGIN_PORT 23291
+#define LOGIN_IP "cho-patate.zapto.org"
+
+enum LoginPacketId //Stored in sf::Uint8
+{
+    LOGIN = 0,
+    SUCCESS = 1,
+    FAIL = 2
+};
+
+enum LoginErrorId //Stored in sf::Uint8
+{
+    NONE = -1,
+    INTERNAL_ERROR = 0,
+    UNKNOWN_USER = 1,
+    BAD_PASSWORD = 2,
+    BAD_REQUEST = 3
+};
+
 
 #endif // MAIN_H_INCLUDED
